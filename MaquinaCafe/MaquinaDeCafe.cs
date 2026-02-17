609e33c9a144a33edb03fd517a145a5929d23630
isaiasdamasoperez <57593188+isaiasdamasoperez@users.noreply.github.com>
2026-02-17 13:10:40 -0400

    Refactor Vaso properties and methods in MaquinaDeCafe

--- MaquinaCafe/MaquinaDeCafe.cs
+++ MaquinaCafe/MaquinaDeCafe.cs
@@ -5,30 +5,14 @@ public class MaquinaDeCafe
 {
     public Cafetera Cafetera { get; private set; } = new Cafetera(0);
 
-    public Vaso VasosPequeno { get; private set; } = new Vaso(0, 0);
-    public Vaso VasosMediano { get; private set; } = new Vaso(0, 0);
-    public Vaso VasosGrande { get; private set; } = new Vaso(0, 0);
+    public Vaso Vaso { get; private set; } = new Vaso(0, 0);
 
     public Azucarero Azucarero { get; private set; } = new Azucarero(0);
 
     public void SetCafetera(Cafetera cafetera) => Cafetera = cafetera;
-    public void SetVasosPequeno(Vaso vaso) => VasosPequeno = vaso;
-    public void SetVasosMediano(Vaso vaso) => VasosMediano = vaso;
-    public void SetVasosGrande(Vaso vaso) => VasosGrande = vaso;
+    public void SetVaso(Vaso vaso) => Vaso = vaso;
     public void SetAzucarero(Azucarero azucarero) => Azucarero = azucarero;
 
-    public Vaso GetTipoDeVaso(string tipo)
-    {
-        tipo = (tipo ?? "").Trim().ToLowerInvariant();
-
-        return tipo switch
-        {
-            "pequeno" => VasosPequeno,
-            "mediano" => VasosMediano,
-            "grande" => VasosGrande,
-            _ => throw new ArgumentException("Tipo de vaso inválido", nameof(tipo))
-        };
-    }
 
     public string GetVasoDeCafe(Vaso vaso, int cantidadVasos, int cucharadasAzucar)
     {
